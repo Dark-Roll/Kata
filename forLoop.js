290a1068d1c65fd949c8fc44101b0c711e7e18ea
Shiou <ai791016ai791016@gmail.com>
2018-05-21 16:13:33 +0800

    add: some animation and JS from ACCESS

---
+++ forLoop.js
@@ -0,0 +1,29 @@
+<script>
+// window.onload = function(){
+//     var img_total =44;
+//         for(i=10;i<img_total+1 ;i++ ){
+//             // document.write('Welcome to Wibibi.<br>'+TestString);
+//             var elem=document.getElementById ('imgPlace');
+//             var img = document.createElement('img');
+//             img.setAttribute('src', "./img/201802_VCO_SAKURAMATURI_LPai-01_"+i+".jpg");
+//             console.log(img);
+            
+//             elem.append(img);
+            
+//             //ToDo :elem problem
+//             //i : 01-10 problem
+//             // Cart disappear
+            
+//             // var elem = document.getElementById('uniqueId');
+//             // var img = document.createElement('img');
+//             // img.setAttribute('src', PGL_BUT_PATH);
+//             // elem.appendChild(img);
+    
+    
+//             // a.write(' \n\
+//             // // document.write('<img src="images/logo.png">');
+//         }
+// }
+    
+
+</script>
